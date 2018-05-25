a6c32266db41b0d15b55473c996b9f0641d0c698
nattis <nattawan.wattanasin@evry.com>
2018-05-25 22:04:38 +0200

    Add files

---
+++ src/main/java/com/example/boot/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.boot.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome To My Spring Boot Demo";
+	}
+}
